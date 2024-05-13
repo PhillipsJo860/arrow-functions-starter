7a8d347bfed710a3e7242353a9ac417e886eedb2
SoggyCereal <PhillipsJo860@northwested.org>
2024-05-13 11:18:15 -0400

    added functions for practice

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-// Student name
-// Current date
+// Joshua Phillips
+// 5/13/24
 // JS Arrow Functions Practice
 
 // View the markdown (.md) file in VS Code for instructions
+
+//First Function--->
+
+// const studentNS = (firstName, schoolName='Bellaire High School') => `Hello ${firstName} from, ${schoolName}!`;
+
+// const firstName = prompt('Please enter your first name');
+// console.log(studentNS(firstName));
+
+//Second Function--->
+
+// const totalCost = (cost1, cost2, cost3) => cost1 + cost2 + cost3;
+
+// const cost1 = 24;
+// const cost2 = 43;
+// const cost3 = 12;
+// console.log(`The total cost is ${totalCost(cost1, cost2, cost3)}`);
+
+//Third Function--->
+
+const funcS = (num = 4) => num * num;
+
+alert.log(`The square of ${num} is equal to ${funcS}`);
